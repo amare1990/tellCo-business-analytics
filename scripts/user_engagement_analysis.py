e20b16f46a44ff4a843f4710445cd9b4b8366903
amare1990 <amaremek@gmail.com>
2024-12-21 21:14:10 +0300

    Add a method to get top 10 users engaged

--- scripts/user_engagement_analysis.py
+++ scripts/user_engagement_analysis.py
@@ -12,5 +12,32 @@ class UserEngagementAnalysis:
         # Initialize database connection parameters from environment variables
         pass
 
+    def aggregate_metrics(self, data):
+      """Aggregate metrics per customer id."""
+      # Rename columns to clarify
+      data = data.rename(columns={
+          'Dur. (ms)': 'session_duration',
+          'Total UL (Bytes)': 'upload_traffic',
+          'Total DL (Bytes)': 'download_traffic'
+      })
+
+      # Derive session_frequency and session_traffic
+      data['session_frequency'] = data.groupby('MSISDN/Number')['Bearer Id'].transform('count')
+      data['session_traffic'] = data['upload_traffic'] + data['download_traffic']
+
+      # Aggregate data per customer
+      agg_data = data.groupby("MSISDN/Number").agg({
+          'session_frequency': 'sum',
+          'session_duration': 'sum',
+          'session_traffic': 'sum'
+      }).reset_index()
+
+      # Get top 10 customers for each metric
+      top_customers = {
+          'session_frequency': agg_data.nlargest(10, 'session_frequency'),
+          'session_duration': agg_data.nlargest(10, 'session_duration'),
+          'session_traffic': agg_data.nlargest(10, 'session_traffic')
+      }
+      return agg_data, top_customers
 
 
