2fff39d17123c12b1f33d5af7ac2ddc3c5d79a0b
amare1990 <amaremek@gmail.com>
2024-12-22 14:10:57 +0300

    Fix bugs in the aggregate_traffic_per_app method by providing exact names of apps

--- scripts/user_engagement_analysis.py
+++ scripts/user_engagement_analysis.py
@@ -41,11 +41,11 @@ class UserEngagementAnalysis:
       return agg_data, top_customers
 
 
-    def normalize_and_cluster(self, agg_data, k=3):
+    def normalize_and_cluster(self, agg_data, k=3, n_init=10):
         """Normalize metrics and perform k-means clustering."""
         scaler = MinMaxScaler()
         normalized_data = scaler.fit_transform(agg_data[['session_frequency', 'session_duration', 'session_traffic']])
-        kmeans = KMeans(n_clusters=k, random_state=42)
+        kmeans = KMeans(n_clusters=k, random_state=4, n_init=n_init)
         agg_data['cluster'] = kmeans.fit_predict(normalized_data)
         return agg_data, kmeans
 
@@ -61,7 +61,7 @@ class UserEngagementAnalysis:
     def aggregate_traffic_per_app(self, data):
         """Aggregate user total traffic per application."""
         applications = {
-            'Social Media': ['Social Media DL (Bytes)', 'Social Media UL (Bytes)'],
+            'Social Media': ["Social Media DL (Bytes)", 'Social Media UL (Bytes)'],
             'Google': ['Google DL (Bytes)', 'Google UL (Bytes)'],
             'Email': ['Email DL (Bytes)', 'Email UL (Bytes)'],
             'YouTube': ['Youtube DL (Bytes)', 'Youtube UL (Bytes)'],
@@ -69,7 +69,27 @@ class UserEngagementAnalysis:
             'Gaming': ['Gaming DL (Bytes)', 'Gaming UL (Bytes)'],
             'Other': ['Other DL (Bytes)', 'Other UL (Bytes)']
         }
-        for application, value in applications.items():
-            app_traffic = data.groupby([value[0]+value[1], 'MSISDN/Number'])['session_traffic'].sum().reset_index()
-        top_users_per_app = app_traffic.sort_values('session_traffic', ascending=False).groupby('application').head(10)
+
+        # Initialize an empty DataFrame for aggregated traffic
+        app_traffic = []
+
+        # Loop through each application and aggregate traffic
+        for app_name, columns in applications.items():
+            data[f"{app_name}_traffic"] = data[columns[0]] + data[columns[1]]
+            app_df = (
+                data.groupby("MSISDN/Number")[[f"{app_name}_traffic"]]
+                .sum()
+                .reset_index()
+            )
+            app_df["application"] = app_name
+            app_df.rename(columns={f"{app_name}_traffic": "session_traffic"}, inplace=True)
+            app_traffic.append(app_df)
+
+        # Concatenate traffic data for all applications
+        app_traffic = pd.concat(app_traffic, ignore_index=True)
+
+        # Get top users per application
+        top_users_per_app = app_traffic.sort_values("session_traffic", ascending=False).groupby("application").head(10)
+
         return app_traffic, top_users_per_app
+
